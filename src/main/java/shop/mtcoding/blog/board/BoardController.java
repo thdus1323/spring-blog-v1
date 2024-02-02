9d00bcbf7353474e19ecbc5f1ce7c3cb70a953ee
thdus <thdus1323@naver.com>
2024-02-02 16:09:32 +0900

    잘하고 싶어요

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -4,12 +4,20 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
 @Controller
 public class BoardController {
 
     private final HttpSession session;
+    private final BoardRepository boardRepository;
+    //데이터 리턴시(RestController or ResponseBody) - 객체를 리턴하면 스프링이 json으로 변환
+    @GetMapping("/api/board/{id}")
+    public @ResponseBody Board apiBoard(@PathVariable int id){
+        return boardRepository.findById(id);
+    }
 
     @GetMapping({ "/", "/board" })
     public String index() {
